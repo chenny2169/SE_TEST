dfa72ad0d847e585926a92548e1c54b538f4dd89
chenny2169 <chenny2169@gmail.com>
2017-11-22 12:13:00 +0800

    111

--- src/min.js
+++
@@ -1,9 +0,0 @@
-// min.js
-// 取得陣列中的最小值
-module.exports = function min(array) {
-  let min = array[0]
-  for (let i = 0; i < array.length; i++) {
-    min = array[i] < min ? array[i] : min
-  }
-  return min
-}
